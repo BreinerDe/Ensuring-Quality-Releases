d4a703fca6572004e487d4ff2eff9e3f001fe699
Dennis Breiner <dennis.breiner@daimler.com>
2022-07-22 08:14:09 +0200

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,19 +7,18 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
+    storage_account_name = "dennistfstorageaccount"
+    container_name       = "dennistfcontainer"
     access_key           = ""
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
+  source               = "../modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
 module "network" {
-  source               = "../../modules/network"
+  source               = "../modules/network"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   virtual_network_name = "${var.virtual_network_name}"
@@ -30,7 +29,7 @@ module "network" {
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
+  source           = "../modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
@@ -39,7 +38,7 @@ module "nsg-test" {
   address_prefix_test = "${var.address_prefix_test}"
 }
 module "appservice" {
-  source           = "../../modules/appservice"
+  source           = "../modules/appservice"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
