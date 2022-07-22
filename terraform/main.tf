3b8b5fd736125d821898c6f6e370e421cb99ac04
Dennis Breiner <dennis.breiner@daimler.com>
2022-07-22 08:15:56 +0200

    add vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,12 @@ terraform {
   }
 }
 module "resource_group" {
-  source               = "../modules/resource_group"
+  source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
 module "network" {
-  source               = "../modules/network"
+  source               = "./modules/network"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   virtual_network_name = "${var.virtual_network_name}"
@@ -29,7 +29,7 @@ module "network" {
 }
 
 module "nsg-test" {
-  source           = "../modules/networksecuritygroup"
+  source           = "./modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
@@ -38,16 +38,27 @@ module "nsg-test" {
   address_prefix_test = "${var.address_prefix_test}"
 }
 module "appservice" {
-  source           = "../modules/appservice"
+  source           = "./modules/appservice"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
   resource_group   = "${module.resource_group.resource_group_name}"
 }
 module "publicip" {
-  source           = "../../modules/publicip"
+  source           = "./modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
   resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "virtual_machine" {
+  source               = "./modules/vm"
+  location             = var.location
+  resource_group       = module.resource_group.resource_group_name
+  application_type     = var.application_type
+  resource_type        = "VM"
+
+  public_ip_address_id = module.public_ip.public_ip_address_id
+  public_subnet_id     = module.network.subnet_id_test
+  admin_username       = "adminuser"
 }
